9455056901de5babdddf6501efe79d7676c117d6
jorge.dominguez <jorge.dominguez@ext.privalia.com>
2018-04-26 16:14:10 +0200

    Video player created

--- app/src/main/java/com/jorgedguezm/exoplayertest/MainActivity.java
+++ app/src/main/java/com/jorgedguezm/exoplayertest/MainActivity.java
@@ -1,13 +1,40 @@
 package com.jorgedguezm.exoplayertest;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+
 public class MainActivity extends AppCompatActivity {
+    SimpleExoPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleExoPlayerView playerView = new SimpleExoPlayerView(this);
+
+        this.createPlayer();
+        playerView.setPlayer(this.player);
+    }
+
+    private void createPlayer() {
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+        this.player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
     }
 }
