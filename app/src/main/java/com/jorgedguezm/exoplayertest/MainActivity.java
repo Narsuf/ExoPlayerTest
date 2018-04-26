95079c9923f53d01d056702b8abc966139300825
Jorge <jorgedguezm@gmail.com>
2018-04-26 20:49:35 +0200

    Video playing correctly!

--- app/src/main/java/com/jorgedguezm/exoplayertest/MainActivity.java
+++ app/src/main/java/com/jorgedguezm/exoplayertest/MainActivity.java
@@ -1,20 +1,27 @@
 package com.jorgedguezm.exoplayertest;
 
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+    SimpleExoPlayerView exoPlayerView;
     SimpleExoPlayer player;
 
     @Override
@@ -22,19 +29,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SimpleExoPlayerView playerView = new SimpleExoPlayerView(this);
+        this.exoPlayerView = findViewById(R.id.playerView);
 
         this.createPlayer();
-        playerView.setPlayer(this.player);
+        this.exoPlayerView.setPlayer(this.player);
+        this.preparePlayer();
+        this.player.setPlayWhenReady(true);
     }
 
     private void createPlayer() {
-        Handler mainHandler = new Handler();
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory =
                 new AdaptiveTrackSelection.Factory(bandwidthMeter);
         TrackSelector trackSelector =
                 new DefaultTrackSelector(videoTrackSelectionFactory);
         this.player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+    }
+
+    private void preparePlayer() {
+        // Measures bandwidth during playback.
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "ExoPlayerTest"), bandwidthMeter);
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(
+                        Uri.parse("http://cdn.s2.eu.nice264.com/converted_work6/" +
+                                "0082c06e504b0a422bf1_6815f2deeb179c29748af42f8cd5ce95.mp4")
+                );
+        // Prepare the player with the source.
+        player.prepare(videoSource);
     }
 }
